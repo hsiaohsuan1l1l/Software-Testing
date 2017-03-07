1af872738f8d070d415c90b7321f1eb7a31dffcb
hsiaohsuan1l1l <hsiaohsuan1l1l@gmail.com>
2017-03-07 18:07:34 +0800

    Declare function for BVTest

--- src/Triangle/triangle.cc
+++ src/Triangle/triangle.cc
@@ -2,5 +2,12 @@
 
 // Return Equilateral or Isosceles or NotATriangle
 string Triangle(int a, int b, int c) {
-    
+    if (a <= 0 || b <= 0 || c <= 0)
+        return "NotATriangle";
+    if (a + b < c || a + c < b || b + c < a)
+        return "NotATriangle";
+    if (a == b == c)
+        return "Equilateral";
+    if (a == b || a == c || b == c)
+        return "Isosceles";
 }
