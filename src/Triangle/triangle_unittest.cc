fbc7ac28fd02ae52f445802359f9e7726c4cbac4
hsiaohsuan1l1l <hsiaohsuan1l1l@gmail.com>
2017-03-08 13:06:40 +0800

    Finish DTBTest

--- src/Triangle/triangle_unittest.cc
+++ src/Triangle/triangle_unittest.cc
@@ -110,3 +110,54 @@ TEST(EdgeTest, StrongNormal) {
 TEST(EdgeTest, StrongRobust) {
     // Similar with tests in ECTest.StrongRobust when choosing boundary
 }
+
+// Decision Table Based Test
+    // c1: a < b + c ?
+    // c2: b < a + c ?
+    // c3: c < a + b ?
+    // c4: a = b ?
+    // c5: a = c ?
+    // c6: b = c ?
+TEST(DTBTest, Rule1) { // c1=False; rc=32
+    EXPECT_EQ("NotATriangle", Triangle(100, 50, 50));
+}
+
+TEST(DTBTest, Rule2) { // c1=True, c2=False; rc=16
+    EXPECT_EQ("NotATriangle", Triangle(50, 100, 50));
+}
+
+TEST(DTBTest, Rule3) { // c1,c2=True, c3=False; rc=8
+    EXPECT_EQ("NotATriangle", Triangle(50, 50, 100));
+}
+
+TEST(DTBTest, Rule4) { // c1,c2,c3,c4,c5,c6=True; rc=1
+    EXPECT_EQ("Equilateral", Triangle(200, 200, 200));
+}
+
+TEST(DTBTest, Rule5) { // c1,c2,c3,c4,c5=True, c6=False; rc=1
+    // Logically Impossible
+}
+
+TEST(DTBTest, Rule6) { // c1,c2,c3,c4,c6=True, c5=False; rc=1
+    // Logically Impossible
+}
+
+TEST(DTBTest, Rule7) { // c1,c2,c3,c4=True, c5,c6=False; rc=1
+    EXPECT_EQ("Isosceles", Triangle(100, 100, 99));
+}
+
+TEST(DTBTest, Rule8) { // c1,c2,c3,c5,c6=True, c4=False; rc=1
+    // Logically Impossible
+}
+
+TEST(DTBTest, Rule9) { // c1,c2,c3,c5=True, c4,c6=False; rc=1
+    EXPECT_EQ("Isosceles", Triangle(100, 99, 100));
+}
+
+TEST(DTBTest, Rule10) { // c1,c2,c3,c6=True, c4,c5=False; rc=1
+    EXPECT_EQ("Isosceles", Triangle(99, 100, 100));
+}
+
+TEST(DTBTest, Rule11) { // c1,c2,c3=True, c4,c5,c6=False; rc=1
+    EXPECT_EQ("Scalene", Triangle(99, 100, 101));
+}
