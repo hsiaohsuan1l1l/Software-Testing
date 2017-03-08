222867ff65e9599e1010a40033d5447677e57d31
hsiaohsuan1l1l <hsiaohsuan1l1l@gmail.com>
2017-03-08 16:23:32 +0800

    Fix the leap year problem

--- src/NextDate/nextdate.cc
+++ src/NextDate/nextdate.cc
@@ -13,6 +13,10 @@ string NextDate(int y, int m, int d) {
     // Distinguish the edge day
     if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
         edgeday = 31;
+    else if (m == 2 && (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)))
+        edgeday = 29;
+    else if (m == 2)
+        edgeday = 28;
     else
         edgeday = 30;
     
