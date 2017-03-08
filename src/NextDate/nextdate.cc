6479d0ce29f2a8d2b211c5654f503f55b0842558
hsiaohsuan1l1l <hsiaohsuan1l1l@gmail.com>
2017-03-08 16:12:42 +0800

    Distinguish the month with 30 or 31 days

--- src/NextDate/nextdate.cc
+++ src/NextDate/nextdate.cc
@@ -4,13 +4,24 @@
 string NextDate(int y, int m, int d) {
     stringstream ss;
     string nextdate, year, month, day;
-    
+    int edgeday;
+
     // Validate the input
     if (y < 1 || m < 1 || m > 12 || d < 1 || d > 31)
         return "InvalidInput";
 
+    // Distinguish the edge day
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+        edgeday = 31;
+    else
+        edgeday = 30;
+    
+    // Validate the edge day
+    if (d > edgeday)
+        return "InvalidInput";
+
     // Generate next date
-    if (d + 1 > 31) {
+    if (d + 1 > edgeday) {
         if (m + 1 > 12) {
             y++;
             m = d = 1;
