7cd109f111568640031d79f57ce317a52fdc510f
hsiaohsuan1l1l <hsiaohsuan1l1l@gmail.com>
2017-03-08 15:21:46 +0800

    Day increment failed

--- src/NextDate/nextdate.cc
+++ src/NextDate/nextdate.cc
@@ -2,5 +2,28 @@
 
 // Return the next date of input date
 string NextDate(int y, int m, int d) {
-    return "";
+    stringstream ss;
+    string nextdate, year, month, day;
+
+    // Generate next date
+    d++;
+
+    // Convert int to string
+    ss << y;
+    ss >> year;
+    ss.str("");
+    ss.clear();
+    ss << m;
+    ss >> month;
+    ss.str("");
+    ss.clear();
+    ss << d;
+    ss >> day;
+    ss.str("");
+    ss.clear();
+
+    // Format the answer
+    nextdate = year + "." + month + "." + day;
+
+    return nextdate;
 }
