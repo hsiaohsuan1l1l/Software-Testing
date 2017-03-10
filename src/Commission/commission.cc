29b3be74a53cecc8e10e26157503e0258edd0091
hsiaohsuan1l1l <hsiaohsuan1l1l@gmail.com>
2017-03-11 01:29:58 +0800

    Failed for invalid and format

--- src/Commission/commission.cc
+++ src/Commission/commission.cc
@@ -2,5 +2,27 @@
 
 // Return the sales value
 string Commission(int l, int s, int b) {
-    return "";
+    stringstream ss;
+    string comm_str;
+    double sales, comm;
+    
+    // Calculate the sales
+    sales = l * 45 + s * 30 + b * 25;
+
+    // Calculate the commission
+    if (sales >= 1800)
+        comm = 0.1 * 1000 + 0.15 * 800 + 0.2 * (sales - 1800);
+    else if (sales >= 1000)
+        comm = 0.1 * 1000 + 0.15 * (sales - 1000);
+    else
+        comm = 0.1 * sales;
+
+    // Format the answer
+    ss << fixed << setprecision(2) << comm;
+    ss >> comm_str;
+    ss.str("");
+    ss.clear();
+    comm_str = "$" + comm_str;
+
+    return comm_str;
 }
