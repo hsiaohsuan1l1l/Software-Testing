d906f00ca0414bbbff7ef3016634322b174bd786
Hsiaohsuan <hsiaohsuan1l1l@gmail.com>
2017-05-24 06:49:36 +0000

    Error for external call

---
+++ src/NextDate/main_klee.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "klee/klee.h"
+
+
+// Return the next date of input date
+int main(void) {
+    int y, m, d, edgeday;
+
+    klee_make_symbolic(&y, sizeof(y), "y");
+    klee_make_symbolic(&m, sizeof(m), "m");
+    klee_make_symbolic(&d, sizeof(d), "d");
+
+    // Validate the input
+    if (y < 1 || m < 1 || m > 12 || d < 1 || d > 31)
+        printf("InvalidInput");
+
+    // Distinguish the edge day
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+        edgeday = 31;
+    else if (m == 2 && (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)))
+        edgeday = 29;
+    else if (m == 2)
+        edgeday = 28;
+    else
+        edgeday = 30;
+    
+    // Validate the edge day
+    if (d > edgeday)
+        printf("InvalidInput");
+
+    // Generate next date
+    if (d + 1 > edgeday) {
+        if (m + 1 > 12) {
+            y++;
+            m = d = 1;
+        } else {
+            m++;
+            d = 1;
+        }
+    } else {
+        d++;
+    }
+
+    printf("%d.%d.%d", y, m, d);
+
+    return 0;
+}
